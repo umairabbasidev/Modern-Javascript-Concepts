f620e30597b6639e6a54a51b02de7608956e12b6
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-02 21:16:01 +0500

    class 4 completed, concept of conversion of datatypes

---
+++ class4_Comparision-of-datatypes.js
@@ -0,0 +1,21 @@
+// topic Comparision of datatypes
+
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 <= 1);  // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+
+
+
+// console.log('2'>1)
+// console.log('02'>1)
+
+// console.log(null>0)  // false
+// console.log(null==0) // false
+// console.log(null>=0) // true 
+// b/c  null js treat null 0 
+
+
