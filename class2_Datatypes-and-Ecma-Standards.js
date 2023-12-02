fec63976070f71ffe0669ac0f43b7598b115d2ac
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-02 13:01:22 +0500

    class 2 completed, concept of datatypes and ecma standard

---
+++ class2_Datatypes-and-Ecma-Standards.js
@@ -0,0 +1,36 @@
+// Topic : Datatypes and ecma standard
+
+"use strict";  // treat all js code as newer version
+
+// alert(3+3);  we are using nodejs, not browser
+
+// code readability should be high
+
+// Documents followed durng this series 
+// mdn 
+// tc39.es 
+
+// Datatypes 
+
+let name = "umair";
+let age = 18;
+let isLoggedIn = true;
+
+// Primitives DataTypes   for memorization point of view (NN,SS,BB,U)
+// null => standalone value
+// number => 2 to power 53;
+// boolean => true/false
+// BigInt  => 
+// string => "", ''
+// symbol => for uniqueness find
+// undefined => 
+
+
+// object 
+console.log(typeof "")
+console.log(typeof Symbol)
+console.log(typeof 12)
+console.log(typeof null)  // object 
+console.log(typeof true)
+console.log(typeof 233n)
+
