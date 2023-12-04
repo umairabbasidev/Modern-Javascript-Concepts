6bf22caab089d16fbe0731d2bd8b8173fe63b8e6
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-04 11:57:21 +0500

    class 8 completd, concept of number in js

---
+++ class8_Numbers-and-Math.js
@@ -0,0 +1,19 @@
+// Topic : Number and math
+
+const score = 400;
+// console.log( score.toString().length); // string 
+
+console.log(score.toFixed(2)) // 400.00
+
+const anotherNumber = 23.8966;
+console.log(anotherNumber.toPrecision(3)) // 23.9
+
+
+// console.log(score)
+// const score2 = new Number(400);
+// console.log(score)
+// console.log(score2)
+
+// console.log(score2.toString());
+
+// console.log(typeof score2);
