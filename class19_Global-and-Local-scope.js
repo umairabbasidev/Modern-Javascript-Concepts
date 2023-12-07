4500c03ffc421afd0e6878f372d57e8e838ddb1a
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-07 11:04:59 +0500

    class 19 completed , concept of nested scope and mini level hoistin

--- class19_Global-and-Local-scope.js
+++ class19_Global-and-Local-scope.js
@@ -7,4 +7,48 @@ if (true) {
 }
 // console.log(a)
 // console.log(b)
-console.log(c);
+// console.log(c);
+
+// __________X________X_______
+// nested scope and mini level hoisting
+
+
+function one(){
+ const userName = "Umair";
+
+ function two() {
+  const website = "Youtube"
+  console.log(userName)
+ }
+ // console.log(website);
+
+ two() 
+}
+
+one() //  website is not defined
+
+
+
+if (true) {
+ const username = "Umair"
+ if (true) {
+  const website = "Youtube"
+  console.log(username)
+ }
+
+ // console.log(website) // website is not defined
+
+}
+
+
+addOne(5) // accesable and ans is 6
+
+function addOne(num){
+return console.log(num+1)
+}
+
+addTwo(8) //not accessable  Cannot access 'addTwo' before initialization
+
+const addTwo= function(num){
+return console.log(num+2)
+}
